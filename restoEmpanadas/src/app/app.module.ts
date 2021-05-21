8d5691708585b070607d01eb9d3856ddf08ebc04
LAPTOP-NKK30NES\Mohamed <abdeloifimohamed@gmail.com>
2021-05-21 21:00:08 +0200

    resolution de conflit

--- restoEmpanadas/src/app/app.module.ts
+++ restoEmpanadas/src/app/app.module.ts
@@ -1,28 +1,24 @@
 // Angular imports
 import { NgModule } from '@angular/core';
-import { AppRoutingModule } from './app-routing.module';
 import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AccueilComponent } from './accueil/accueil.component';
 import { FooterComponent } from './footer/footer.component';
 import { HeaderComponent } from './header/header.component';
 import { MenuComponent } from './menu/menu.component';
-<<<<<<< HEAD
 import { PacksComponent } from './packs/packs.component';
 import { EmpanadasComponent } from './empanadas/empanadas.component';
 import { BoissonsComponent } from './boissons/boissons.component';
 import { DessertsComponent } from './desserts/desserts.component';
 
-=======
-import { AppRoutingModule } from './app-routing.module';
->>>>>>> 99f87a45933ec62381bd5a3efed79d9102ae9551
 
 
 
 
 
 @NgModule({
-  
+
   declarations: [
     AppComponent,
     AccueilComponent,
@@ -37,10 +33,6 @@ import { AppRoutingModule } from './app-routing.module';
   imports: [
     BrowserModule,
     AppRoutingModule
-<<<<<<< HEAD
-=======
- 
->>>>>>> 99f87a45933ec62381bd5a3efed79d9102ae9551
   ],
   providers: [],
   bootstrap: [AppComponent]
