63f63ce290fa5619311643cc4b0498c9062035a6
LAPTOP-NKK30NES\Mohamed <abdeloifimohamed@gmail.com>
2021-05-21 16:55:29 +0200

    mise en place du rooting

--- restoEmpanadas/src/app/app.module.ts
+++ restoEmpanadas/src/app/app.module.ts
@@ -1,19 +1,27 @@
+// Angular imports
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+// Component
 import { AppComponent } from './app.component';
+import { AccueilComponent } from './accueil/accueil.component';
 import { FooterComponent } from './footer/footer.component';
 import { HeaderComponent } from './header/header.component';
+// Routing
+import { AppRoutingModule } from './app-routing.module';
+import { MenuComponent } from './menu/menu.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
+    AccueilComponent,
+    MenuComponent,
     FooterComponent,
     HeaderComponent
     ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
